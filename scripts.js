5a62bf227174efcd4e53064d0b596775fc44cbbd
IRIS <irisofdefi@gmail.com>
2025-10-12 00:59:44 +0100

    finish building the expense tracker bot

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const addExpenseBtn = document.querySelector(".add-expense-btn");
 const expenseList = document.querySelector(".expense-list");
-const totalExpenses = document.querySelector(".total-expense h3");
+const totalExpenses = document.querySelector(".total-expenses h3");
 
-let expense = [];
+let expenses = [];
 let total = 0;
 
 function renderExpenses() {
   let html = "";
-  expense.forEach((expense) => {
+  expenses.forEach(expense => {
     html += `
   <div class="expense-item">
       <div class="expense-item-description">${expense.description}</div>
@@ -17,7 +17,7 @@ function renderExpenses() {
  `;
   });
   expenseList.innerHTML = html;
-  totalExpenses.innerText = `Total Expenses: $`;
+  totalExpenses.innerText = `Total Expenses: ${total}`;
 }
 function addExpense() {
   const description = prompt("Enter Expense Description");
@@ -28,10 +28,24 @@ function addExpense() {
       description: description,
       amount: amount,
     };
-    expense.push(expense);
+    expenses.push(expense);
     total += amount;
     renderExpenses();
   }
 }
 addExpenseBtn.addEventListener("click", addExpense);
-function deleteExpenses() {}
+
+
+function deleteExpense(index) {
+ total -= expenses[index].amount;
+ expenses.splice(index,1)
+ renderExpenses();
+}
+expenseList.addEventListener("click", function(event){
+ if(event.target.classList.contains("delete-expense-btn")){
+  const index = Array.from(event.target.parentNode.children)
+  .indexOf(event.target.parentNode)
+  deleteExpense(index);
+}
+});
+
